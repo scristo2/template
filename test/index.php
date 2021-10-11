85d654b107737868ce17f1d8a6d0c7f543fc6fbb
scristo2 <sergio_colino_c@hotmail.com>
2021-10-11 18:53:09 +0200

    update code 11/10/21

--- test/index.php
+++ test/index.php
@@ -1,2 +1,21 @@
 <?php 
-echo $_SERVER['HTTP_USER_AGENT'];
+class person{
+   
+    function __construct($name, $surname)
+    {
+        $this->name = $name;
+        $this->surname = $surname;
+    }
+
+    function saludar(){
+        return "my name is $this->name $this->surname";
+    }
+}
+
+
+$sergio = new person('Sergio', 'Cristobal');
+echo $sergio->saludar();
+if(isset($_POST['username'])){
+
+    echo PHP_EOL . "the username is " . $_POST['username'];
+}
