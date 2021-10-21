a5eeda0b791495f2dcff14da0916faf6fcfe20b2
scristo2 <sergio_colino_c@hotmail.com>
2021-10-22 00:03:21 +0200

    update code 22/10/21

--- src/principal/menu/login/index.php
+++ src/principal/menu/login/index.php
@@ -1,10 +1,17 @@
 <div class="App-content-temporal-divs App-content-temporal-login">
-    <div class="App-content-temporal-login-title"><h1>Log In</h1></div>
-    <hr>
+    
     <div class="App-content-temporal-login-formDiv">
         <form id="formLogin" class="App-content-temporal-login-form">
-            <input type="email" name="emailLogin" class="App-content-temporal-login-form-input" placeholder="Enter your email" required>
+            <div class="insideFormLogin insideFormLogin2"><p>Email:</p></div>
+            <input type="email" name="emailLogin" class="App-content-temporal-login-form-input insideFormLogin" required>
+            <div class="insideFormLogin insideFormLogin2"><p>Password:</p></div>
+            <input type="password" name="passwordLogin" minlength="4" required  class="App-content-temporal-login-form-input insideFormLogin">
+            <div class="App-content-temporal-login-form-input-divCheck insideFormLogin">
+                <input type="checkbox" name="checkLogin" class="App-content-temporal-login-form-check">
+                <p>Remember sesion</p>
+            </div>
+            <input type="button" value="Reset password" class="insideFormLogin"  id="App-content-temporal-login-form-reset">
+            <input type="button" class="insideFormLogin" id="App-content-temporal-login-form-input-submit" value="Log In">
         </form>
     </div>
-    <p>ssss</p>
 </div>
